5fb1256e7ca8e97fc371b1ec890ed4f79699f768
SyedMujtaba1 <91568328+SyedMujtaba1@users.noreply.github.com>
2024-09-22 15:08:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! Production site used for this lab\n'
+  const msg = 'Hello Node! Production site used for this lab\n this live is updated it is tested we can live now'
   res.end(msg);
 });
 
